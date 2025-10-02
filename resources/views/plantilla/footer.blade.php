7283ce88c5d6f67620b6e7eaf263cf5e3fcb6892
Anyelo Bustos <sen.programador@gmail.com>
2025-10-02 13:33:38 -0300

    Inicial: Sistema de Ventas Laravel - Plantilla modularizada con AdminLTE

---
+++ resources/views/plantilla/footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="app-footer">
+  <!--begin::To the end-->
+  <div class="float-end d-none d-sm-inline">Sistema Venta v1.0</div>
+  <!--end::To the end-->
+  <!--begin::Copyright-->
+  <strong>
+    Copyright &copy; {{ date('Y') }}&nbsp;
+    <span class="text-decoration-none">Desarrollado por Anyelo Bustos Galdames</span>
+  </strong>
+  - Todos los derechos reservados.
+  <!--end::Copyright-->
+</footer>
